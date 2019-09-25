c2f0114449b3353c5bfd4a2bdfeb5b1ae6289329
Michael Stegeman <michael@stegeman.me>
2019-09-25 13:10:09 -0800

    Add client-side plugin code to library. (#49)

--- index.js
+++ index.js
@@ -8,20 +8,24 @@
 
 const Action = require('./lib/action');
 const Adapter = require('./lib/adapter');
+const AddonManagerProxy = require('./lib/addon-manager-proxy');
 const {APIHandler, APIRequest, APIResponse} = require('./lib/api-handler');
 const Constants = require('./lib/constants');
 const Database = require('./lib/database');
 const Deferred = require('./lib/deferred');
 const Device = require('./lib/device');
 const Event = require('./lib/event');
+const IpcSocket = require('./lib/ipc');
 const Notifier = require('./lib/notifier');
 const Outlet = require('./lib/outlet');
+const PluginClient = require('./lib/plugin-client');
 const Property = require('./lib/property');
 const Utils = require('./lib/utils');
 
 module.exports = {
   Action,
   Adapter,
+  AddonManagerProxy,
   APIHandler,
   APIRequest,
   APIResponse,
@@ -30,8 +34,10 @@ module.exports = {
   Deferred,
   Device,
   Event,
+  IpcSocket,
   Notifier,
   Outlet,
+  PluginClient,
   Property,
   Utils,
   getVersion: () => require('./package.json').version,
