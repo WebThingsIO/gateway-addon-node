d9d14441c1bd00ab0d35e3e89ce73654642f9c0e
Tim Hellhake <tim-hellhake@users.noreply.github.com>
2020-11-10 00:47:52 +0100

    Use !== instead of !=

--- src/property.ts
+++ src/property.ts
@@ -228,19 +228,19 @@ export class Property<T> {
       const numberValue = (<number> <unknown>value);
 
       // eslint-disable-next-line no-undefined
-      if (this.minimum != undefined && numberValue < this.minimum) {
+      if (this.minimum !== undefined && numberValue < this.minimum) {
         reject(`Value less than minimum: ${this.minimum}`);
         return;
       }
 
       // eslint-disable-next-line no-undefined
-      if (this.maximum != undefined && numberValue > this.maximum) {
+      if (this.maximum !== undefined && numberValue > this.maximum) {
         reject(`Value greater than maximum: ${this.maximum}`);
         return;
       }
 
       // eslint-disable-next-line no-undefined
-      if (this.multipleOf != undefined &&
+      if (this.multipleOf !== undefined &&
         numberValue / this.multipleOf -
         Math.round(numberValue / this.multipleOf) !== 0) {
         // note that we don't use the modulus operator here because it's
