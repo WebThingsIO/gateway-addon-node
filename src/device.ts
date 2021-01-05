d83b1cbfe8527db248ae5d01b24591c1b50993bd
Tim Hellhake <tim-hellhake@users.noreply.github.com>
2021-01-05 22:28:04 +0100

    Fix camelcase issue

--- src/device.ts
+++ src/device.ts
@@ -139,7 +139,7 @@ export class Device {
     return this.pinPattern;
   }
 
-  setpinPattern(pinPattern: string): void {
+  setPinPattern(pinPattern: string): void {
     this.pinPattern = pinPattern;
   }
 
