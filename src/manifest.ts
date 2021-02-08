97827ae917d132e0d0e6f4697b99f4ea548265ff
Tim Hellhake <tim-hellhake@users.noreply.github.com>
2021-02-08 23:41:32 +0100

    Add types for the manifest.json file

--- src/manifest.ts
+++ src/manifest.ts
@@ -9,3 +9,39 @@ export interface Moziot<T> {
   schema: Record<string, unknown>;
   config: T;
 }
+
+export interface ManifestFile {
+  manifest_version: number;
+  id: string;
+  name: string;
+  short_name: string;
+  version: string;
+  description: string;
+  homepage_url: string;
+  license: string;
+  author: string;
+  gateway_specific_settings: GatewaySpecificSettings;
+  options?: Options;
+  content_scripts?: ContentScript[];
+  web_accessible_resources?: string[];
+}
+export interface GatewaySpecificSettings {
+  webthings: WebthingsSettings;
+}
+
+export interface WebthingsSettings {
+  exec?: string;
+  strict_min_version: string;
+  strict_max_version: string;
+  primary_type: 'adapter' | 'notifier' | 'extension';
+}
+
+export interface Options {
+  default?: Record<string, unknown>;
+  schema: Record<string, unknown>;
+}
+
+interface ContentScript {
+  css?: string[];
+  js?: string[];
+}
